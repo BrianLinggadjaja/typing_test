1690279fdea8458ed893dc0737964998a7d86fde
brianlinggadjaja <brianlinggadjaja@outlook.com>
2020-11-23 23:16:36 -0800

    Moved event listeners to helper.js

--- js/entry.js
+++ js/entry.js
@@ -3,30 +3,6 @@
     // Check if any leaderboard scores are saved; render proper view
     checkForStoredLeaderboard()
 
-    // Race Mode Actions
-    const raceButton = document.querySelector('#raceButton')
-    const raceOneMinute = document.querySelector('#raceOne')
-    const raceTwoMinutes = document.querySelector('#raceTwo')
-    const raceThreeMinutes = document.querySelector('#raceThree')
-    const raceFiveMinutes = document.querySelector('#raceFive')
-    const raceCancel = document.querySelector('#raceCancel')
-
-    raceButton.addEventListener('click', toggleRaceModal)
-    raceOneMinute.addEventListener('click', setMode.bind(this, 'race', 1))
-    raceTwoMinutes.addEventListener('click', setMode.bind(this, 'race', 2))
-    raceThreeMinutes.addEventListener('click', setMode.bind(this, 'race', 3))
-    raceFiveMinutes.addEventListener('click', setMode.bind(this, 'race', 5))
-    raceCancel.addEventListener('click', toggleRaceModal)
-
-    // Pace Mode Action
-    const paceButton = document.querySelector('#paceButton')
-
-    paceButton.addEventListener('click', setMode.bind(this, 'pace'))
-
-    // TestActions
-    const confirmScore = document.querySelector('#confirmScore')
-    const resetTest = document.querySelector('#resetTest')
-
-    confirmScore.addEventListener('click', confirmTypingTestScore)
-    resetTest.addEventListener('click', resetTypingTest)
+    // Initalize all event listeners
+    initalizeEventListeners()
 })()
--- js/helper.js
+++ js/helper.js
@@ -33,6 +33,40 @@ let WPMScaling = 1.25
 
 
 /*
+    * Event Listeners
+    */
+
+function initalizeEventListeners() {
+    // Race Mode Actions
+    const raceButton = document.querySelector('#raceButton')
+    const raceOneMinute = document.querySelector('#raceOne')
+    const raceTwoMinutes = document.querySelector('#raceTwo')
+    const raceThreeMinutes = document.querySelector('#raceThree')
+    const raceFiveMinutes = document.querySelector('#raceFive')
+    const raceCancel = document.querySelector('#raceCancel')
+
+    raceButton.addEventListener('click', toggleRaceModal)
+    raceOneMinute.addEventListener('click', setMode.bind(this, 'race', 1))
+    raceTwoMinutes.addEventListener('click', setMode.bind(this, 'race', 2))
+    raceThreeMinutes.addEventListener('click', setMode.bind(this, 'race', 3))
+    raceFiveMinutes.addEventListener('click', setMode.bind(this, 'race', 5))
+    raceCancel.addEventListener('click', toggleRaceModal)
+
+    // Pace Mode Action
+    const paceButton = document.querySelector('#paceButton')
+
+    paceButton.addEventListener('click', setMode.bind(this, 'pace'))
+
+    // Test Actions
+    const confirmScore = document.querySelector('#confirmScore')
+    const resetTest = document.querySelector('#resetTest')
+
+    confirmScore.addEventListener('click', confirmTypingTestScore)
+    resetTest.addEventListener('click', resetTypingTest)
+}
+
+
+/*
     * Render
     */
 
